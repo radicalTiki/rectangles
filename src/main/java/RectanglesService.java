95314b106af96a21cabf65143e9d6fe4ac4d6721
Matt Gleason <gleason.matt@protonmail.com>
2022-09-09 09:00:08 -0400

    updates to challenge

--- src/main/java/RectanglesService.java
+++ src/main/java/RectanglesService.java
@@ -49,18 +49,31 @@ class RectanglesService {
 
             Point[] points = new Point[2];
             
-            //this rectangle is to the left of the check rectangle and height is above check triangle
-            //points are on the same y axis
-            if (this.x < r.x && this.y + this.height < r.y + r.height) {
-                points[0] = new Point(x_axis_points[1], y_axis_points[1]);
-                points[1] = new Point(x_axis_points[2], y_axis_points[1]);
+            //check if this rectangle shares a edge with the check rectangle
+            if (this.x == r.x || this.y == r.y || this.x + this.width == r.x + r.width ||
+                    this.y + this.height == r.y + r.height) {
+                //shared edge logic, no more than 1 point
             }
+            else {
+                //2 point intersection
+                int maxDifference = Math.max(this.x + this.width, r.x + r.width) -
+                        Math.min(this.x + this.width, r.x + r.width);
+                int minDifference = Math.max(this.x, r.x) - Math.min(this.x, r.x);
+
+                if (this.x < r.x && this.y + this.height < r.y + r.height) {
+                    points[0] = new Point(x_axis_points[1], y_axis_points[1]);
+                    points[1] = new Point(x_axis_points[2], y_axis_points[1]);
+                }
+            }
+
             //this rectangle is to the right of the check rectangle and height is above check triangle
             //points are on the same
             else if (this.x > r.x && this.y + this.height < r.y + r.height) {
 
             }
-            
+            else if (this.x > r.x && this.y + this.height < r.y + r.height) {
+
+            }
 
 
             return points;
--- src/main/java/RectanglesServiceTest.java
+++ src/main/java/RectanglesServiceTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.awt.*;
@@ -8,7 +9,12 @@ import java.awt.*;
 public class RectanglesServiceTest {
 
     @Test
-    public void testGetIntersection() {
+    public void testGetIntersectionFor2Points() {
+        //  xxxxxxxxxxx
+        //  x         x
+        //  x   xxxxx x
+        //  xxxxxxxxxxx
+        //      xxxxx
         RectanglesService.Rectangle rect1 = new RectanglesService.Rectangle(0, 1, 10, 5);
         RectanglesService.Rectangle rect2 = new RectanglesService.Rectangle(4, 0, 5, 2);
 
@@ -16,6 +22,10 @@ public class RectanglesServiceTest {
         RectanglesService.Point[] points = rect1.getIntersectionPoints(rect2);
 
         System.out.println("IntersectionPoints: " + points[0] + " " + points[1]);
+        Assert.assertEquals(points[0].x, 4);
+        Assert.assertEquals(points[0].y, 1);
+        Assert.assertEquals(points[1].x, 9);
+        Assert.assertEquals(points[1].y, 1);
     }
 
 //    private boolean containsPoint(RectanglesService.PlotPoint[] plotPoints, double xAxisPoint, double yAxisPoint) {
